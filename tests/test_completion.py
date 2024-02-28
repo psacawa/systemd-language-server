46e6578ec4ec8019596d86bd1a49884fe6b92428
Paweł Sacawa <pawel@sacawa.net>
2024-02-28 04:39:15 +0100

    Comprehensive completion tests

--- tests/test_completion.py
+++ tests/test_completion.py
@@ -1,6 +1,7 @@
 from concurrent.futures import TimeoutError
 from pathlib import Path
 
+import pytest
 from lsprotocol.types import (
     INITIALIZE,
     TEXT_DOCUMENT_COMPLETION,
@@ -61,49 +62,126 @@ def client_open(client: LanguageServer, path: Path, text: str | None = None):
     )
 
 
-def test_basic(client_server_pair: ClientServerPair):
+from dataclasses import dataclass
+
+
+@dataclass
+class CompletionTestParams:
+    filename: str | None
+    text: str
+    position: tuple[int, int]
+    contains_completion_labels: list[str]
+    excludes_completion_labels: list[str]
+
+
+#  WorkingDirectory - systmed.exec.xml
+#  ExecStart - systmed.service.xml
+#  KillMode - systmed.kill.xml
+
+no_section_test = CompletionTestParams(
+    "test.service",
+    "\n\n",
+    (1, 0),
+    ["Description", "ExecStart", "WorkingDirectory", "WantedBy", "KillMode"],
+    [],
+)
+unit_section_test = CompletionTestParams(
+    "test.service",
+    "[Unit]\n\n",
+    (1, 0),
+    ["Description"],
+    ["ExecStart", "WorkingDirectory", "WantedBy", "KillMode"],
+)
+install_section_test = CompletionTestParams(
+    "test.service",
+    "[Install]\n\n",
+    (1, 0),
+    ["WantedBy"],
+    ["Description", "WorkingDirectory", "ExecStart", "KillMode"],
+)
+service_section_test = CompletionTestParams(
+    "test.service",
+    "[Service]\n\n",
+    (1, 0),
+    ["ExecStart", "WorkingDirectory", "KillMode"],
+    ["Description", "WantedBy"],
+)
+socket_section_test = CompletionTestParams(
+    "test.socket",
+    "[Socket]\n\n",
+    (1, 0),
+    ["ListenStream", "WorkingDirectory", "KillMode"],
+    ["ExecStart", "Description", "WantedBy"],
+)
+mount_section_test = CompletionTestParams(
+    "test.mount",
+    "[Mount]\n\n",
+    (1, 0),
+    ["What", "Where", "WorkingDirectory", "KillMode"],
+    ["ExecStart", "Description", "WantedBy"],
+)
+timer_section_test = CompletionTestParams(
+    "test.timer",
+    "[Timer]\n\n",
+    (1, 0),
+    ["OnCalendar"],
+    ["ExecStart", "Description", "WantedBy"],
+)
+
+#  completing "Exec"... in [Service] works as intended
+service_directive_test = CompletionTestParams(
+    "test.service",
+    "[Service]\nExec\n",
+    (1, 4),
+    ["ExecStart", "ExecStartPre", "ExecStartPost"],
+    ["WorkingDirectory", "KillMode"],
+)
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        no_section_test,
+        unit_section_test,
+        install_section_test,
+        service_section_test,
+        socket_section_test,
+        mount_section_test,
+        timer_section_test,
+        service_directive_test,
+    ],
+)
+def test_completion(client_server_pair: ClientServerPair, params: CompletionTestParams):
     client, server = client_server_pair
-    print(client)
-    print(server)
 
     datadir = Path(__file__).parent / "data"
-    service_unit_file = datadir / "test.service"
-    uri = service_unit_file.as_uri()
+    assert params.filename is not None
+    unit_file = datadir / params.filename
+    uri = unit_file.as_uri()
 
     client_init(client, datadir)
-    client_open(client, service_unit_file)
-    completion_list: CompletionList = client.lsp.send_request(
-        TEXT_DOCUMENT_COMPLETION,
-        params=CompletionParams(
-            text_document=TextDocumentIdentifier(uri=uri),
-            position=Position(1, 0),
-            context=None,
-        ),
-    ).result(timeout=1)
-    assert isinstance(completion_list, CompletionList)
-    labels = [i.label for i in completion_list.items]
-    assert "Description" in labels
-    assert "ExecStart" not in labels
+    client_open(client, unit_file)
 
     client.lsp.notify(
         TEXT_DOCUMENT_DID_CHANGE,
         params=DidChangeTextDocumentParams(
             text_document=VersionedTextDocumentIdentifier(version=1, uri=uri),
-            content_changes=[
-                TextDocumentContentChangeEvent_Type2(text="[Service]\n\n")
-            ],
+            content_changes=[TextDocumentContentChangeEvent_Type2(text=params.text)],
         ),
     )
 
-    completion_list = client.lsp.send_request(
+    completion_list: CompletionList = client.lsp.send_request(
         TEXT_DOCUMENT_COMPLETION,
         params=CompletionParams(
             text_document=TextDocumentIdentifier(uri=uri),
-            position=Position(1, 0),
+            position=Position(*params.position),
             context=None,
         ),
     ).result(timeout=1)
     assert isinstance(completion_list, CompletionList)
     labels = [i.label for i in completion_list.items]
-    assert "Description" not in labels
-    assert "ExecStart" in labels
+    print(labels)
+    for contained_label in params.contains_completion_labels:
+        assert contained_label in labels
+    for excluded_label in params.excludes_completion_labels:
+        assert not excluded_label in labels
